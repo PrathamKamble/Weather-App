058a92e5d96888748aee3566c308587eb31a180a
Pratham Kamble <prathamkamble727@gmail.com>
2023-12-03 16:59:47 +0530

    project testing

---
+++ script.js
@@ -0,0 +1,55 @@
+const latitude = document.querySelector('.lat');
+const longitude = document.querySelector('.long');
+
+const locationName = document.querySelector('.loc');
+const windSpeed = document.querySelector('.windSpeed');
+const humidity = document.querySelector('.humidity');
+const timeZone = document.querySelector('.timeZone');
+const pressure = document.querySelector('.Pressure');
+const windDirection = document.querySelector('.windDirc');
+const uvIndex = document.querySelector('.uvIdx');
+const feelsLike = document.querySelector('.Temp');
+const fetchBtn = document.getElementById('fetchBtn');
+
+const apiKey = "1e43862694e6e758ff1ddc5385dd612f";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+
+async function getLocation(position) {
+    await checkWeather(position.coords.latitude, position.coords.longitude);
+};  
+function failed() {
+    console.log('failed to get location');
+};
+
+fetchBtn.addEventListener('click', async () => {
+    navigator.geolocation.getCurrentPosition(getLocation, failed);
+    document.getElementById('landing-page').style.display = 'none';
+    document.getElementById('data-page').style.display = 'block';
+});
+
+async function checkWeather(lat, long) {
+    const response = await fetch(apiUrl + `&lat=${lat}&lon=${long}&appid=${apiKey}`)
+    
+    let data = await response.json();
+    console.log(data);
+    
+    latitude.innerHTML = `Lat : ${lat}`;
+
+    longitude.innerHTML = `Long : ${long}`;
+
+    locationName.innerHTML = `Location : ${data.name}`;
+    
+    windSpeed.innerHTML = `Wind Speed : ${data.wind.speed} Kmph`;
+    
+    humidity.innerHTML = `Humidity : ${data.main.humidity}`;
+    
+    timeZone.innerHTML = `Time Zone : GMT ${data.timezone}`;
+    
+    pressure.innerHTML = `Pressure : ${data.main.pressure} atm`;
+    
+    windDirection.innerHTML = `Wind Direction : ${data.wind.deg}`;
+    
+    uvIndex.innerHTML = `UV Index : ${data.main.temp}`;
+    
+    feelsLike.innerHTML = `Feels like : ${data.main.feels_like}°`;
+}
